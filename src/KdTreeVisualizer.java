ee8acce172d813d35d3cada62b9e121affaee37f
HJS <407410014@qq.com>
2017-11-06 20:06:11 +0800

    safe point commit

--- src/KdTreeVisualizer.java
+++ src/KdTreeVisualizer.java
@@ -18,23 +18,23 @@ public class KdTreeVisualizer {
     public static void main(String[] args) {
         RectHV rect = new RectHV(0.0, 0.0, 1.0, 1.0);
         StdDraw.enableDoubleBuffering();
-//        KdTree kdtree = new KdTree();
-        PointSET kdtree = new PointSET();
+        KdTree kdtree = new KdTree();
+//        PointSET kdtree = new PointSET();
         while (true) {
             if (StdDraw.isMousePressed()) {
                 double x = StdDraw.mouseX();
                 double y = StdDraw.mouseY();
-                StdOut.printf("%8.6f %8.6f\n", x, y);
+//                StdOut.printf("%8.6f %8.6f\n", x, y);
                 Point2D p = new Point2D(x, y);
                 if (rect.contains(p)) {
-                    StdOut.printf("%8.6f %8.6f\n", x, y);
+//                    StdOut.printf("%8.6f %8.6f\n", x, y);
                     kdtree.insert(p);
                     StdDraw.clear();
                     kdtree.draw();
                     StdDraw.show();
                 }
             }
-            StdDraw.pause(20);
+            StdDraw.pause(200);
         }
 
     }
